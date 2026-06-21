645f6d9aeed8d4c72754c67b61fd7471807b3619
damasrui92-code <damasrui92@gmail.com>
2026-06-21 19:58:42 +0100

    config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+const DEFAULT_CONFIG = {
+
+    houseAmount: 65000,
+
+    emergencyGoal: 500000,
+
+    studioGoal: 300000,
+
+    netWorthGoal: 1500000,
+
+    ruiLimit: 20000,
+
+    allocation: {
+        emergency: 15,
+        investment: 5,
+        mobility: 30,
+        food: 30,
+        rui: 10,
+        opportunities: 10
+    }
+
+};
