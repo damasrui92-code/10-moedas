cdc28557d80cec71e8de157c428c1230cd4d5f98
damasrui92-code <damasrui92@gmail.com>
2026-06-21 20:04:31 +0100

    wallets.js
    Added a default wallets array with initial values.

---
+++ js/wallets.js
@@ -0,0 +1,59 @@
+const DEFAULT_WALLETS = [
+
+    {
+        id: "WAL-001",
+        name: "🛡️ Emergência",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-002",
+        name: "🏦 Investimento",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-003",
+        name: "🚗 Mobilidade",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-004",
+        name: "🍽️ Alimentação",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-005",
+        name: "🎮 Rui",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-006",
+        name: "🚀 Oportunidades",
+        balance: 0,
+        income: 0,
+        expense: 0
+    },
+
+    {
+        id: "WAL-007",
+        name: "🎙️ Estúdio",
+        balance: 0,
+        income: 0,
+        expense: 0
+    }
+
+];
